ed63140a324a68a4b9b9d33de1db97642bd39a9f
legowiifun <arath5@gmail.com>
2026-02-17 17:20:18 -0700

    Switch to using GEODE_EVENT_EXPORT_NORES

--- src/api.cpp
+++ src/api.cpp
@@ -3,11 +3,10 @@
 #include "api.hpp"
 #include "Stat.cpp"
 
-geode::Result<void> stat_display_api::addStatEvent(std::string start, std::string end, std::string id, int value) {
+void stat_display_api::addStatEvent(std::string start, std::string end, std::string id, int value) {
     log::debug("Recieving addStatEvent: start={}", start);
     StatDisplayBaseGameLayer* layer = static_cast<StatDisplayBaseGameLayer*>(GJBaseGameLayer::get());
     otherStats++;
     g_strings.insert(std::make_pair<int, std::pair<std::string, std::string>>(static_cast<int>(Stat::OTHER) + otherStats, { start, end }));
     layer->createStatsNode(layer->m_fields->m_statsContainer, Stat::OTHER, id, value);
-    return Ok();
 }
